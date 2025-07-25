cb0eed8eae3fdf0e94a365692217a13128783ff7
Mattheaus Hoffman <mattheaus.hoffman@educ.rec.br>
2025-07-25 16:50:45 -0300

    refactor: login service clear

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 export interface Login {
   id?: string;
@@ -12,11 +10,4 @@ export interface Login {
   providedIn: 'root',
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:3000/login';
-
-  constructor(private http: HttpClient) {}
-
-  createLogin(login: Login): Observable<any> {
-    return this.http.post<any>(this.apiUrl, login);
-  }
 }
